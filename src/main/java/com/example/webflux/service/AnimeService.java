62b7413b1a6a39d4e90684087965bec181b3e9e4
GabrielSilva <gabrielbarros@alu.ufc.br>
2023-01-18 10:42:28 -0300

    Adding error when entity cannot be found

--- src/main/java/com/example/webflux/service/AnimeService.java
+++ src/main/java/com/example/webflux/service/AnimeService.java
@@ -3,7 +3,9 @@ package com.example.webflux.service;
 import com.example.webflux.domain.Anime;
 import com.example.webflux.repository.AnimeRepositoty;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -18,7 +20,12 @@ public class AnimeService {
     }
 
     public Mono<Anime> findById(int id){
-        return animeRepositoty.findById(id);
+        return animeRepositoty.findById(id)
+                .switchIfEmpty(monoResponseNotFoundException());
+    }
+
+    public <T> Mono<T> monoResponseNotFoundException(){
+        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND,"Anime not found"));
     }
 
 }
